652744aada75b2280a1ce0d6a104cfa1775a7b83
kingsman001 <krishiv988@gmail.com>
2026-05-04 19:40:15 +0530

    fix: unified build and deploy and corrected gitignore

--- .open-next/server-functions/default/.next/server/app/api/stats/route.js
+++
@@ -1,8 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/stats/route.js")
-R.c("server/chunks/[root-of-the-server]__0pl~v2i._.js")
-R.c("server/chunks/node_modules_next_dist_12gp6q5._.js")
-R.c("server/chunks/[root-of-the-server]__0_~_1-l._.js")
-R.c("server/chunks/node_modules_0gnf0qa._.js")
-R.c("server/chunks/_next-internal_server_app_api_stats_route_actions_0dv_g.a.js")
-R.m(18816)
-module.exports=R.m(18816).exports
--- .open-next/server-functions/default/.next/server/chunks/_next-internal_server_app_api_leetcode_route_actions_0ao8f9s.js
+++
@@ -1,3 +0,0 @@
-module.exports=[43254,(e,o,d)=>{}];
-
-//# sourceMappingURL=_next-internal_server_app_api_leetcode_route_actions_0ao8f9s.js.map
