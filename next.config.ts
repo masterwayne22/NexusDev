3cc31e394fdd9dc6355cd81514a5a3537143f895
kingsman001 <krishiv988@gmail.com>
2026-05-05 21:58:17 +0530

    removed cloudflare files

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 import type { NextConfig } from "next";
 import path from "path";
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
-
-if (process.env.NODE_ENV === "development") {
-  initOpenNextCloudflareForDev();
-}
 
 const nextConfig: NextConfig = {
   /* Next.js 16 Turbopack Configuration */
